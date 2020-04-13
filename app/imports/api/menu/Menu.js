0ad751592573c058968c5ff45d8c062b7ab2b99a
zjlin <zijunlin32@gmail.com>
2020-04-12 16:58:52 -1000

    Menu collection done

---
+++ app/imports/api/menu/Menu.js
@@ -0,0 +1,20 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Define a Mongo collection to hold the data. */
+const Menu = new Mongo.Collection('Menu');
+
+/** Define a schema to specify the structure of each document in the collection. */
+const MenuSchema = new SimpleSchema({
+  itemName: String,
+  price: Number,
+  description: String,
+  ingredients: String,
+  }, { tracker: Tracker });
+
+/** Attach this schema to the collection. */
+Menu.attachSchema(MenuSchema);
+
+/** Make the collection and schema available to other code. */
+export { Menu, MenuSchema };
