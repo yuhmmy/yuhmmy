26db2da88c036d8f90371d65c2dc81fa283eb935
Tina Loos <tinaloos@hawaii.edu>
2020-04-15 17:26:26 -1000

    Implemented Landing Page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
+        <div className='yUHmmy-landing-background'>
+          <Grid container stackable centered columns={3}>
+            <Grid.Column textAlign='center'>
+              <Icon size='huge' name='users' inverted/>
+              <Header as='h1' inverted> UH Manoa Restaurants </Header>
+              <Header as='h3' inverted>Interface for restaurants and their customer! Looking for somewhere to eat on
+                campus? Easily search for the type of food that you are looking for!</Header>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon size='huge' name='file alternate' inverted/>
+              <Header as='h1' inverted>In a Rush? </Header>
+              <Header as='h3' inverted>You came to the right place! Order beforehand & your food will *magically* appear
+                at your table</Header>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon size='huge' name='calendar check' inverted/>
+              <Header as='h1' inverted>$$ </Header>
+              <Header as='h3' inverted> Quickly pay and tip on-app for limited interaction between wait staff and
+                customers</Header>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }
