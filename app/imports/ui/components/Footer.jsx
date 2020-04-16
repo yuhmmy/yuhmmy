4b9ae75b11650b14f11e9e33b98da687a9bc7e3c
spjy <spjy@hawaii.edu>
2020-04-16 12:52:27 -1000

    style: remove unused variable

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
     return (
       <footer className="footer">
         <div className="ui center aligned container">
