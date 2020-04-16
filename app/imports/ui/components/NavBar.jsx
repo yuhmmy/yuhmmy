c6f430eea1a1445b665ff76a89e9b61b4cb3988b
zjlin <zijunlin32@gmail.com>
2020-04-16 12:04:36 -1000

    removed lint errors

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import {Menu, Dropdown, Header, Icon} from 'semantic-ui-react';
+import { Menu, Dropdown, Header, Icon } from 'semantic-ui-react';
 import { Roles } from 'meteor/alanning:roles';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
@@ -11,9 +11,7 @@ class NavBar extends React.Component {
   render() {
     const menuStyle = { marginBottom: '10px' };
     return (
-
       <Menu style={menuStyle} attached="top" borderless inverted>
-
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
           <Header inverted as='h1'>yUHmmy</Header>
         </Menu.Item>
@@ -23,7 +21,6 @@ class NavBar extends React.Component {
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
             <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
         ) : ''}
-
         <Menu.Item position="right" >
             {this.props.currentUser === '' ? (
                 [
@@ -34,7 +31,7 @@ class NavBar extends React.Component {
                         <Icon name='add user'/> Sign Up
                     </Menu.Item>,
                 ]
-            ): (
+            ) : (
                 <Dropdown text={this.props.currentUser} pointing="top right" icon={'user'}>
                     <Dropdown.Menu>
                         <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to="/Signout"/>
@@ -42,7 +39,6 @@ class NavBar extends React.Component {
                 </Dropdown>
             )}
         </Menu.Item>
-
       </Menu>
     );
   }
