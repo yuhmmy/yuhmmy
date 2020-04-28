4ae810eafdad3879bea744e795a5ce7cb564801a
Tina Loos <tinaloos@hawaii.edu>
2020-04-27 23:30:24 -1000

    Update RestaurantAdd.jsx

--- app/imports/ui/pages/RestaurantAdd.jsx
+++ app/imports/ui/pages/RestaurantAdd.jsx
@@ -4,9 +4,9 @@ import AutoForm from 'uniforms-semantic/AutoForm';
 import TextField from 'uniforms-semantic/TextField';
 import LongTextField from 'uniforms-semantic/LongTextField';
 import SubmitField from 'uniforms-semantic/SubmitField';
-import NumField from 'uniforms-semantic//NumField';
+// import NumField from 'uniforms-semantic//NumField';
 // import HiddenField from 'uniforms-semantic/HiddenField';
-import ErrorsField from 'uniforms-semantic/ErrorsField';
+// import ErrorsField from 'uniforms-semantic/ErrorsField';
 import swal from 'sweetalert';
 // import { Meteor } from 'meteor/meteor';
 import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
@@ -14,29 +14,29 @@ import SimpleSchema from 'simpl-schema';
 import { Restaurants } from '../../api/restaurant/Restaurant';
 
 /** Create a schema to specify the structure of the data to appear in the form. */
-const makeSchema = new SimpleSchema({
+const formSchema = new SimpleSchema({
   restaurantName: String,
-  description: String,
-  rating: String,
-  location: String,
-  phoneNumber: {
-    type: String,
-    required: false,
-  },
-  restaurantAddress: String,
-  owner: String,
-  logo: String,
-  hours: String,
-  website: String,
-  menu: {
-    type: String,
-    required: false,
-  },
-  image: {
-    type: String,
-    required: false,
-  },
-  diet: String,
+  restaurantAddress: Object,
+  'restaurantAddress.street': String,
+  'restaurantAddress.city': String,
+  'restaurantAddress.state': String,
+  'restaurantAddress.zipCode': String,
+  restaurantImage: String,
+  restaurantDesc: String,
+  restaurantOwner: String,
+  restaurantOrders: { type: Object, required: false },
+  'restaurantOrders.kitchenQueue': Array,
+  'restaurantOrders.kitchenQueue.$': Object,
+  'restaurantOrders.kitchenQueue.$.kitchenName': String,
+  'restaurantOrders.kitchenQueue.$.preference': String,
+  'restaurantOrders.drinkQueue': Array,
+  'restaurantOrders.drinkQueue.$': Object,
+  'restaurantOrders.drinkQueue.$.drinkName': String,
+  'restaurantOrders.drinkQueue.$.preference': String,
+  'restaurantOrders.waitQueue': Array,
+  'restaurantOrders.waitQueue.$': Object,
+  'restaurantOrders.waitQueue.$.waitName': String,
+  'restaurantOrders.waitQueue.$.preference': String,
 });
 
 /** Renders the Page for adding a document. */
@@ -45,13 +45,11 @@ class RestaurantAdd extends React.Component {
   /** On submit, insert the data. */
   submit(data, formRef) {
     const {
-      restaurantName, description, rating, location, phoneNumber, restaurantAddress, logo, hours,
-      website, menu, image, diet,
+      restaurantName, restaurantAddress, restaurantImage, restaurantDesc, restaurantOwner, restaurantOrders,
     } = data;
     // const owner = Meteor.user().username;
     Restaurants.insert({
-          restaurantName, restaurantAddress, hours, menu, phoneNumber, location, image,
-          logo, website, description, rating, diet,
+          restaurantName, restaurantAddress, restaurantImage, restaurantDesc, restaurantOwner, restaurantOrders,
         },
         (error) => {
           if (error) {
@@ -66,7 +64,6 @@ class RestaurantAdd extends React.Component {
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   render() {
     let fRef = null;
-    const formSchema = makeSchema;
     return (
         <Grid container centered>
           <Grid.Column>
@@ -80,60 +77,29 @@ class RestaurantAdd extends React.Component {
                              placeholder='Your Restaurant Name'/>
                 </Grid.Column>
                 <Grid.Column>
-                  <TextField label='Restaurant Phone Number:' name='phoneNumber' required={false}
-                             placeholder='Your Restaurant Phone Number'/>
-                </Grid.Column>
-                <Grid.Column>
-                  <TextField label='Restaurant Hours:' name='hours'
-                             placeholder='08:00AM - 08:00PM'/>
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row columns={3}>
-                <Grid.Column>
                   <TextField label='Restaurant Address:' name='restaurantAddress'
                              placeholder='Your Restaurant Address'/>
                 </Grid.Column>
                 <Grid.Column>
-                  <TextField label='Restaurant Owner:' name='owner'
-                             placeholder='Your Restaurant Owner'/>
-                </Grid.Column>
-                <Grid.Column>
-                  <TextField label='Restaurant Website:' name='site'
-                             placeholder='www.RestaurantSite.com'/>
+                  <TextField label='Restaurant Image:' name='restaurantImage'
+                             placeholder='RestaurantImage.jpg'/>
                 </Grid.Column>
               </Grid.Row>
               <Grid.Row columns={3}>
                 <Grid.Column>
-                  <TextField label='Restaurant Logo:' name='logo'
-                             placeholder='www.RestaurantLogo.jpg'/>
+                  <TextField label='Restaurant Owner:' name='restaurantOwner'
+                             placeholder='Your Restaurant Owner'/>
                 </Grid.Column>
                 <Grid.Column>
-                  <TextField label='Restaurant Photo:' name='image' required={false}
-                             placeholder='www.RestaurantPic.jpg'/>
+                  <TextField label='Restaurant Orders:' name='restaurantOrders' required={false}
+                             placeholder='Your Restaurant Orders'/>
                 </Grid.Column>
                 <Grid.Column>
-                  <TextField label='Restaurant Menu URL:' name='menu' required={false}
-                             placeholder='www.RestaurantMenu.com'/>
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row columns={2}>
-                <Grid.Column>
-                  <NumField label='Rating' name='rating'
-                            placeholder='Rate Your Restaurant'/>
-                </Grid.Column>
-                <Grid.Column>
-                  <TextField label='Dietary Options' name='diet' required={false}
-                             placeholder='List Dietary Options'/>
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row>
-                <Grid.Column>
-                  <LongTextField label='Restaurant Description:' name='description'
-                                 placeholder='Describe Your Website...'/>
+                  <LongTextField label='Restaurant Description:' name='restaurantDesc'
+                                 placeholder='Describe Your Restaurant...'/>
                 </Grid.Column>
               </Grid.Row>
               <SubmitField value='Submit'/>
-              <ErrorsField/>
             </AutoForm>
           </Grid.Column>
         </Grid>
