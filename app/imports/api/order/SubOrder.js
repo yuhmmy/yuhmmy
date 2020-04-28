3cbaa474fc474a257574acdbb1bae5b89c360042
Keenan Lee <49707796+knolee@users.noreply.github.com>
2020-04-27 20:29:07 -1000

    separated order to two collections

--- app/imports/api/order/SubOrder.js
+++ app/imports/api/order/SubOrder.js
@@ -3,19 +3,17 @@ import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
 
 /** Define a Mongo collection to hold the data. */
-const Orders = new Mongo.Collection('Orders');
+const SubOrders = new Mongo.Collection('SubOrders');
 
 /** Define a schema to specify the structure of each document in the collection. */
-const OrdersSchema = new SimpleSchema({
-  orderRestaurantId: String,
-  orderItemId: String,
-  orderQuantity: SimpleSchema.Integer,
-  orderPreference: String,
-  orderIsFinished: Boolean,
+const SubOrdersSchema = new SimpleSchema({
+  orderId: String,
+  subOrderQuantity: SimpleSchema.Integer,
+  subOrderIsFinished: Boolean,
 }, { tracker: Tracker });
 
 /** Attach this schema to the collection. */
-Orders.attachSchema(OrdersSchema);
+SubOrders.attachSchema(SubOrdersSchema);
 
 /** Make the collection and schema available to other code. */
-export { Orders, OrdersSchema };
+export { SubOrders, SubOrdersSchema };
