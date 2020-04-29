eb1baf5cf4927b043792b6720379cd70057554a5
Tina Loos <tinaloos@hawaii.edu>
2020-04-28 17:23:44 -1000

    Improved Functionality of Edit Restaurant Page

--- app/imports/ui/pages/RestaurantEdit.jsx
+++ app/imports/ui/pages/RestaurantEdit.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Grid, Loader, Header, Segment } from 'semantic-ui-react';
 import swal from 'sweetalert';
-import { AutoForm, ErrorsField, HiddenField, NumField, SelectField, SubmitField, TextField } from 'uniforms-semantic';
+import { AutoForm, ErrorsField, HiddenField, LongTextField, SubmitField, TextField } from 'uniforms-semantic';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
-import { Stuffs, StuffSchema } from '../../api/stuff/Stuff';
+import { Restaurants, RestaurantSchema } from '../../api/restaurant/Restaurant';
 
 /** Renders the Page for editing a single document. */
 class RestaurantEdit extends React.Component {
 
   /** On successful submit, insert the data. */
   submit(data) {
-    const { name, quantity, condition, _id } = data;
-    Stuffs.update(_id, { $set: { name, quantity, condition } }, (error) => (error ?
+    const { restaurantName, restaurantAddress, restaurantImage, restaurantDesc, _id } = data;
+    Restaurants.update(_id, {
+      $set: { restaurantName, restaurantAddress, restaurantImage, restaurantDesc } }, (error) => (error ?
         swal('Error', error.message, 'error') :
         swal('Success', 'Item updated successfully', 'success')));
   }
@@ -30,11 +31,14 @@ class RestaurantEdit extends React.Component {
         <Grid container centered>
           <Grid.Column>
             <Header as="h2" textAlign="center">Edit Stuff</Header>
-            <AutoForm schema={StuffSchema} onSubmit={data => this.submit(data)} model={this.props.doc}>
+            <AutoForm schema={RestaurantSchema} onSubmit={data => this.submit(data)} model={this.props.doc}>
               <Segment>
-                <TextField name='name'/>
-                <NumField name='quantity' decimal={false}/>
-                <SelectField name='condition'/>
+                <TextField label='Restaurant Name:' name='restaurantName' placeholder='McDonald&apos;s'/>
+                <TextField label='Restaurant Address:' name='restaurantAddress'
+                           placeholder='2121 S King St, Honolulu, HI 96826'/>
+                <TextField label='Restaurant Image:' name='restaurantImage' placeholder='McD.jpg'/>
+                <LongTextField label='Restaurant Description:' name='restaurantDesc'
+                               placeholder='We have the finest nuggets that will make you say mmmmm I&apos;m lovin it'/>
                 <SubmitField value='Submit'/>
                 <ErrorsField/>
                 <HiddenField name='owner'/>
@@ -47,7 +51,7 @@ class RestaurantEdit extends React.Component {
 }
 
 /** Require the presence of a Stuff document in the props object. Uniforms adds 'model' to the props, which we use. */
-EditStuff.propTypes = {
+RestaurantEdit.propTypes = {
   doc: PropTypes.object,
   model: PropTypes.object,
   ready: PropTypes.bool.isRequired,
@@ -58,9 +62,9 @@ export default withTracker(({ match }) => {
   // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
   const documentId = match.params._id;
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe('Stuff');
+  const subscription = Meteor.subscribe('Restaurants');
   return {
-    doc: Stuffs.findOne(documentId),
+    doc: Restaurants.findOne(documentId),
     ready: subscription.ready(),
   };
 })(RestaurantEdit);
