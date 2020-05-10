109dc93bc77904822ab7da87bf6ddd344cc149b2
Tina Loos <tinaloos@hawaii.edu>
2020-05-10 04:36:24 -1000

    Fixed ESLint and formatting

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -7,8 +7,6 @@ import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
-// import ListStuff from '../pages/ListStuff';
-// import ListStuffAdmin from '../pages/ListStuffAdmin';
 import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
@@ -39,10 +37,6 @@ class App extends React.Component {
               <ProtectedRoute path="/add" component={RestaurantAdd}/>
               <ProtectedRoute path="/edit/:_id" component={RestaurantEdit}/>
               <ProtectedRoute path="/profile" component={Profile}/>
-              {/* <ProtectedRoute path="/list" component={ListStuff}/>
-              <ProtectedRoute path="/add" component={AddStuff}/>
-              <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
-              <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/> */}
               <ProtectedRoute path="/signout" component={Signout}/>
               <Route component={NotFound}/>
             </Switch>
--- app/imports/ui/pages/RestaurantOrder.jsx
+++ app/imports/ui/pages/RestaurantOrder.jsx
@@ -1,43 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Header, Card, Loader } from 'semantic-ui-react';
+import { Grid, Header, Loader } from 'semantic-ui-react';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Orders } from '../../api/order/Order';
 import OrderCard from '../components/OrderCard';
-import { Restaurants } from '../../api/restaurant/Restaurant';
 import { SubOrders } from '../../api/order/SubOrder';
 
 class RestaurantOrder extends React.Component {
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
+
   renderPage() {
     const orderIds = this.props.order.map(index => index._id);
-    const orderArray = SubOrders.find({ orderId:{
-      $in: orderIds
-  }}).fetch();
-  console.log(orderArray);
+    const orderArray = SubOrders.find({
+      orderId: {
+        $in: orderIds,
+      },
+    }).fetch();
+    console.log(orderArray);
     return (
-      <div className="order">
-            <div className="order-menu">
-              <Header as="h2" inverted>Kitchen Queue</Header>
-              <Grid columns={3} padded>
-                <Grid.Row>
-                  {
-                    orderArray.map(orders => (
+        <div className="order">
+          <div className="order-menu">
+            <Header as="h2" inverted>Kitchen Queue</Header>
+            <Grid columns={3} padded>
+              <Grid.Row>
+                {
+                  orderArray.map(orders => (
                       <OrderCard
-                        subOrder={orders}
+                          subOrder={orders}
+                          key={orders._id}
                       />
-                    ))
-                  }
-                </Grid.Row>
-              </Grid>
-            </div>
-      </div>
+                  ))
+                }
+              </Grid.Row>
+            </Grid>
+          </div>
+        </div>
     );
   }
 }
+
 RestaurantOrder.propTypes = {
   order: PropTypes.array,
   subOrders: PropTypes.array,
@@ -48,9 +52,9 @@ export default withTracker(({ match }) => {
   const restaurantId = match.params._id;
   const subscription = Meteor.subscribe('SubOrders');
   const subscription2 = Meteor.subscribe('Orders');
-    return {
-      order: Orders.find({ orderRestaurantId: restaurantId }).fetch(),
-      subOrders: SubOrders.find({}).fetch(),
-      ready: subscription.ready() && subscription2.ready(),
-    };
+  return {
+    order: Orders.find({ orderRestaurantId: restaurantId }).fetch(),
+    subOrders: SubOrders.find({}).fetch(),
+    ready: subscription.ready() && subscription2.ready(),
+  };
 })(RestaurantOrder);
